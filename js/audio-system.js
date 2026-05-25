119862a73eb28c3aeb4ff57968b9b7299b24925b
marketmechanicxg <grizzyxixi@gmail.com>
2026-05-25 13:11:19 +0700

    Update audio-system.js

--- js/audio-system.js
+++ js/audio-system.js
@@ -14,8 +14,8 @@
 const playlist = [
   {
     id:        'song1',
-    title:     'Sampai Jadi Debu',
-    artist:    'Banda Neira',
+    title:     'My Live All Mine',
+    artist:    'Mitski',
     file:      'music/song1.mp3',
     cover:     'assets/music-cover/song1.jpeg',
     startTime: 0,       // seconds — where playback begins
